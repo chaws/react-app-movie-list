7fdc057fdddc4c7c36b5c596b4397557cbed05ff
Charles Oliveira <charles.oliveira.sp11@gmail.com>
2025-12-09 01:05:36 -0300

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Favorites from './pages/Favorites.jsx'
+import Navbar from './components/Navbar.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Home />
+        <div>
+            <Navbar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </main>
+        </div>
     </>
   )
 }
